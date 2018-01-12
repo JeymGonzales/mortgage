4e8bf4bd34df809989d6a38ff10194accbebe8e2
erson <jeff.cruz@carbondigital.com.ph>
2018-01-12 15:32:07 +0800

    erson branch

--- src/js/question.js
+++ src/js/question.js
@@ -4,6 +4,7 @@ export class Question {
 
 	constructor() {
         this.next = document.querySelectorAll('.js-next');
+        this.back = document.querySelectorAll('.js-back');
 	}
 
 	init() {
@@ -23,26 +24,55 @@ export class Question {
             let test = re.test(email.toLowerCase());
 
             if(!test) {
-                console.log('not valid')
-            } 
+                console.log('not valid');
+            } else {
+                console.log('valid');
+            }
         }
 
-        for(let i = 0; i<fields.length;i++) {
-            if(fields[i].value != '') {
-                if(fields[i].getAttribute('name') === 'email') {
-                    validateEmail(fields[i].value)
-                } else {
-                    curr.classList.add('hideQuestion');
-                    let next = this.getAttribute('data-to')
-                    document.getElementById(next).classList.add('showQuestion');
-                }
-            } 
+        let nextQuestion = function() {
+            curr.classList.remove('showQuestion');
+            curr.classList.add('hideQuestion');
+            let next = this.getAttribute('data-to');
+            console.log('next: '+next);
+            document.getElementById(next).classList.remove('hideQuestion');
+            document.getElementById(next).classList.add('showQuestion');
         }
+
+        if (fields.length > 0) {
+            console.log('with fields');
+            for(let i = 0; i<fields.length;i++) {
+                if(fields[i].value != '') {
+                    if(fields[i].getAttribute('name') === 'email') {
+                        validateEmail(fields[i].value)
+                    } else {
+                        nextQuestion.bind(this)();
+                    }
+                } 
+            }            
+        } else {
+            console.log('no fields');
+            nextQuestion.bind(this)();
+        }
+
+
+
+    }
+
+    previous() {
+        let curr =  this.parentNode.parentNode;
+        let back = this.getAttribute('data-from');
+        console.log('back: '+back);
+        curr.classList.remove('showQuestion');
+        curr.classList.add('hideQuestion');
+        document.getElementById(back).classList.remove('hideQuestion');
+        document.getElementById(back).classList.add('showQuestion');
     }
 
     bindClick() {
         for(let i = 0; i<this.next.length; i++) {
             this.next[i].addEventListener('click', this.validate);
+            this.back[i].addEventListener('click', this.previous);
         }
     }
 
